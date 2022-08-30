1109d2df2a486e720a41b1f93585069e3f812a62
KyeongYeongDEV <yeong7098@naver.com>
2022-08-30 13:48:14 +0900

    Done

--- app/src/app.js
+++ app/src/app.js
@@ -2,12 +2,13 @@
 
 const express = require("express");
 const app = express();
-const home = require("./routs/home/rout");
+const home = require("./routes/home/route");
 
 app.set("views", "./views");
 app.set("view engine","ejs");
 
 app.use("/", home);
 
+app.use(express.static(`${__dirname}/public`));
 
 module.exports = app;
---
+++ app/src/public/js/home/login.js
@@ -0,0 +1,8 @@
+"use-script";
+
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
+
+console.log(id);
+console.log("hello");
