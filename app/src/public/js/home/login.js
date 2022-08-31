cae2ce752868ba0dd1fc5b07867082d3b2f5be1b
KyeongYeongDEV <yeong7098@naver.com>
2022-08-31 21:54:37 +0900

    Done; '

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -4,5 +4,19 @@ const id = document.querySelector("#id"),
     psword = document.querySelector("#psword"),
     loginBtn = document.querySelector("button");
 
-console.log(id);
-console.log("hello");
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req ={
+        id: id.value,
+        psword: psword.value,
+    };
+
+    fetch("/login",{
+        method: "POST",
+        headers:{
+            "Content-Type" : "applicationjson"
+        },
+        body: JSON.stringify(req),
+    });   
+}
